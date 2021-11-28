315a22a8de23f2f26db68cd7cf50ead06464a5fa
HalaMohammed1256 <48032047+HalaMohammed1256@users.noreply.github.com>
2021-11-28 11:41:34 +0200

    add pod file to the project and install Alamofire, IQKeyboardManagerSwift, Kingfisher and ReachabilitySwift Create Genaric API Services  using alamofire and generic decodable type remove storyboard

--- Recipe-Search-iOS/Network/APIServices.swift
+++ Recipe-Search-iOS/Network/APIServices.swift
@@ -1,8 +1,51 @@
-//
-//  APIServices.swift
-//  Recipe-Search-iOS
-//
-//  Created by Hala on 26/11/2021.
-//
 
-import Foundation
+
+
+import Alamofire
+
+enum Result<T>{
+    case success(T)
+    case failure(Error)
+}
+
+
+class ApiServices{
+    
+    static let instance = ApiServices()
+    
+    func getResponse<T:Decodable>(url: String, paramters: Parameters?, headers: HTTPHeaders?, method: HTTPMethod,  completion: @escaping (Result<T>) -> ()){
+        
+        
+        AF.request(url, method: method, parameters: paramters, encoding: URLEncoding.default, headers: headers).responseJSON{ (response) in
+
+            do{
+                
+                switch response.result{
+                case .success(let value):
+                    
+                    guard let jsonData = try? JSONSerialization.data(withJSONObject: value, options: []) else {
+                        let error = NSError(domain: "Json serialization data error", code: 401, userInfo: [ NSLocalizedDescriptionKey :  NSLocalizedString("Unwrapping error", value: "Json serialization data error", comment: "")
+                        ])
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    let decoder = JSONDecoder()
+                    let data = try decoder.decode(T.self, from: jsonData)
+                    
+                    completion(.success(data))
+                    
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+                
+            }catch{
+                completion(.failure(error))
+            }
+            
+        }
+        
+    }
+}
+
+
